2a31706ed2fd71a697e6c4d517bc35ed123df069
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-21 08:04:45 -0700

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Food Tracker</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
